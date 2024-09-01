7430406420d4d4cb88c41619735867c87440ffa0
Nicolas Dartayeta <63559546+nicolasdartayeta@users.noreply.github.com>
2024-09-01 14:19:27 -0300

    hooks3

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,10 +1,10 @@
 import { Elysia } from "elysia";
 import { logger } from "@server/logger";
-import swagger from "@elysiajs/swagger";
+// import swagger from "@elysiajs/swagger";
 
 const app = new Elysia()
   .use(logger())
-  .use(swagger())
+  // .use(swagger())
   .get("/", () => "Hola bobo")
   .listen(3000);
 
