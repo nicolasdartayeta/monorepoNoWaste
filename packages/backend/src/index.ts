a49dadda0868a7126a2fb466c7cedb507c70185d
Nicolas Dartayeta <63559546+nicolasdartayeta@users.noreply.github.com>
2024-09-01 14:17:02 -0300

    hooks3

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -5,7 +5,8 @@ import swagger from "@elysiajs/swagger";
 const app = new Elysia()
   .use(logger())
   .use(swagger())
-  .get("/", () => "Hola bobo").listen(3000);
+  .get("/", () => "Hola bobo")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
--- packages/backend/src/logger.ts
+++ packages/backend/src/logger.ts
@@ -1,128 +1,125 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Elysia from "elysia"
-import * as pc from "picocolors"
+import Elysia from "elysia";
+import * as pc from "picocolors";
 
 interface Writer {
-  write: (message: string) => void
+  write: (message: string) => void;
 }
 
 const consoleWriter: Writer = {
-    write(message: string) {
-      console.log(message)
-    }
-}
+  write(message: string) {
+    console.log(message);
+  },
+};
 
 interface Options {
-    logIP?: boolean,
-    writer?: Writer
+  logIP?: boolean;
+  writer?: Writer;
 }
-
 
 export const logger = (options?: Options) => {
-    const { write } = options?.writer || consoleWriter
-    return new Elysia()
-        .onRequest((ctx) => {
-            ctx.store = { ...ctx.store, beforeTime: Bun.nanoseconds() }
-        })
+  const { write } = options?.writer || consoleWriter;
+  return new Elysia()
+    .onRequest((ctx) => {
+      ctx.store = { ...ctx.store, beforeTime: Bun.nanoseconds() };
+    })
 
-        .onBeforeHandle({ as: "global" }, (ctx) => {
-            ctx.store = { ...ctx.store, beforeTime: Bun.nanoseconds() }
-        })
-        .onAfterHandle({ as: "global" }, ({ request, store }) => {
-            const logStr: string[] = []
-            if (options !== undefined && options.logIP) {
-                if (request.headers.get("X-Forwarded-For")) {
-                    logStr.push(`[${pc.cyan(request.headers.get("X-Forwarded-For"))}]`)
-                }
-            }
+    .onBeforeHandle({ as: "global" }, (ctx) => {
+      ctx.store = { ...ctx.store, beforeTime: Bun.nanoseconds() };
+    })
+    .onAfterHandle({ as: "global" }, ({ request, store }) => {
+      const logStr: string[] = [];
+      if (options !== undefined && options.logIP) {
+        if (request.headers.get("X-Forwarded-For")) {
+          logStr.push(`[${pc.cyan(request.headers.get("X-Forwarded-For"))}]`);
+        }
+      }
 
-            logStr.push(methodString(request.method))
+      logStr.push(methodString(request.method));
 
-            logStr.push(new URL(request.url).pathname)
-            const beforeTime: number = (store as any).beforeTime;
+      logStr.push(new URL(request.url).pathname);
+      const beforeTime: number = (store as any).beforeTime;
 
-            logStr.push(durationString(beforeTime))
+      logStr.push(durationString(beforeTime));
 
-            write(logStr.join(" "))
-        })
-        .onError({ as: "global" }, ({ request, error, store }) => {
-            const logStr: string[] = []
+      write(logStr.join(" "));
+    })
+    .onError({ as: "global" }, ({ request, error, store }) => {
+      const logStr: string[] = [];
 
-            logStr.push(pc.red(methodString(request.method)))
+      logStr.push(pc.red(methodString(request.method)));
 
-            logStr.push(new URL(request.url).pathname)
+      logStr.push(new URL(request.url).pathname);
 
-            logStr.push(pc.red("Error"))
+      logStr.push(pc.red("Error"));
 
-            if ("status" in error) {
-                logStr.push(String(error.status))
-            }
+      if ("status" in error) {
+        logStr.push(String(error.status));
+      }
 
-            logStr.push(error.message)
-            const beforeTime: number = (store as any).beforeTime;
+      logStr.push(error.message);
+      const beforeTime: number = (store as any).beforeTime;
 
-            logStr.push(durationString(beforeTime))
+      logStr.push(durationString(beforeTime));
 
-            write(logStr.join(" "))
-        })
-}
-
+      write(logStr.join(" "));
+    });
+};
 
 function durationString(beforeTime: number): string {
-    const now = Bun.nanoseconds();
-    const nanoseconds = now - beforeTime;
+  const now = Bun.nanoseconds();
+  const nanoseconds = now - beforeTime;
 
-    let timeMessage: string = "";
+  let timeMessage: string = "";
 
-    if (nanoseconds >= 1e9) {
-        const seconds = (nanoseconds / 1e9).toFixed(2);
-        timeMessage = `| ${seconds}s`;
-    } else if (nanoseconds >= 1e6) {
-        const durationInMilliseconds = (nanoseconds / 1e6).toFixed(0); // Convert to milliseconds
-        timeMessage = `| ${durationInMilliseconds}ms`;
-    } else if (nanoseconds >= 1e3) {
-        const durationInMicroseconds = (nanoseconds / 1e3).toFixed(0); // Convert to microseconds
-        timeMessage = `| ${durationInMicroseconds}µs`;
-    } else {
-        timeMessage = `| ${nanoseconds}ns`;
-    }
+  if (nanoseconds >= 1e9) {
+    const seconds = (nanoseconds / 1e9).toFixed(2);
+    timeMessage = `| ${seconds}s`;
+  } else if (nanoseconds >= 1e6) {
+    const durationInMilliseconds = (nanoseconds / 1e6).toFixed(0); // Convert to milliseconds
+    timeMessage = `| ${durationInMilliseconds}ms`;
+  } else if (nanoseconds >= 1e3) {
+    const durationInMicroseconds = (nanoseconds / 1e3).toFixed(0); // Convert to microseconds
+    timeMessage = `| ${durationInMicroseconds}µs`;
+  } else {
+    timeMessage = `| ${nanoseconds}ns`;
+  }
 
-    return timeMessage;
+  return timeMessage;
 }
 
-
 function methodString(method: string): string {
-    switch (method) {
-        case "GET":
-            // Handle GET request
-            return pc.white("GET")
+  switch (method) {
+    case "GET":
+      // Handle GET request
+      return pc.white("GET");
 
-        case "POST":
-            // Handle POST request
-            return pc.yellow("POST")
+    case "POST":
+      // Handle POST request
+      return pc.yellow("POST");
 
-        case "PUT":
-            // Handle PUT request
-            return pc.blue("PUT")
+    case "PUT":
+      // Handle PUT request
+      return pc.blue("PUT");
 
-        case "DELETE":
-            // Handle DELETE request
-            return pc.red("DELETE")
+    case "DELETE":
+      // Handle DELETE request
+      return pc.red("DELETE");
 
-        case "PATCH":
-            // Handle PATCH request
-            return pc.green("PATCH")
+    case "PATCH":
+      // Handle PATCH request
+      return pc.green("PATCH");
 
-        case "OPTIONS":
-            // Handle OPTIONS request
-            return pc.gray("OPTIONS")
+    case "OPTIONS":
+      // Handle OPTIONS request
+      return pc.gray("OPTIONS");
 
-        case "HEAD":
-            // Handle HEAD request
-            return pc.magenta("HEAD")
+    case "HEAD":
+      // Handle HEAD request
+      return pc.magenta("HEAD");
 
-        default:
-            // Handle unknown request method
-            return method
-    }
+    default:
+      // Handle unknown request method
+      return method;
+  }
 }
