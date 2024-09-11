94d017407328e4f09e56a32d5316eb8a7b7d4131
Nicolas Dartayeta <63559546+nicolasdartayeta@users.noreply.github.com>
2024-09-11 11:18:49 -0300

    authentication

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,13 +3,29 @@ import { logger } from "./controllers/logger";
 import swagger from "@elysiajs/swagger";
 import { loginController } from "./controllers/login";
 import { userController } from "./controllers/users";
+import jwt from "@elysiajs/jwt";
 
 const app = new Elysia()
+  .use(
+    jwt({
+      name: "jwt",
+      secret: Bun.env.JWT_SECRET as string,
+    }),
+  )
   .use(logger())
   .use(swagger())
   .use(loginController)
-  .use(userController)
-  .get("/", () => "Hola bobina")
+  .guard(
+    {
+      async beforeHandle({ jwt, cookie: { auth } }) {
+        const user = await jwt.verify(auth.value);
+
+        if (!user) return "Authenticate first";
+      },
+    },
+    (app) => app.use(userController).get("/", () => "Hola bobina"),
+  )
+
   .listen(3000);
 
 console.log(
