89f168f59bcbb56360336d43c3cf69d9ffeda666
SantiagoCastano03 <santiagocasmdp@gmail.com>
2024-09-11 23:33:16 -0300

    mas

--- packages/backend/src/controllers/commerce.ts
+++ packages/backend/src/controllers/commerce.ts
@@ -1,9 +1,9 @@
 import { Elysia } from "elysia";
-import { addCommerce } from "@server/src/models/commerceFunctions";
+import { addCommerce, getAllCommerces, deleteCommerce } from "@server/src/models/commerceFunctions";
 import { commerceInsertDTO } from "@server/src/types";
 
 export const commerceController = new Elysia ({prefix: "/commerce" })
-    .post("/add",
+    .post("/",
         async ({body}) => {
             const newCommerce = body;
             const result = await addCommerce(newCommerce);
@@ -11,8 +11,42 @@ export const commerceController = new Elysia ({prefix: "/commerce" })
         },
         {
             body: commerceInsertDTO,
-            detail: "Insert a new commerce",
-            tags: ["commerces"]
+            detail:{
+                summary: "Insert a new commerce",
+                tags: ["commerces"]
+            },  
         }
     )
-    .get("/",() => "QUE PASA TITAN");
+    .get("/",
+        async () => {
+            return await getAllCommerces();
+        },
+        {
+            detail:{
+                summary: "Get all commerces in DB",
+                tags: ["commerces"]
+            }
+        }
+    )
+    .delete("/:id",
+        async ({params : {id}}) => {
+            const result = await deleteCommerce(id);
+            return {eliminado: result};
+        },
+        {
+            detail: {
+                summary: "Delete a commerce",
+                tags: ["commerces"]
+            }
+        }
+    )
+    /**
+    .put("/:id",
+        async ({body}) => {
+            const updateCommerce = body;
+        },
+        {
+            body: commerceInsertDTO
+        }
+    )
+    */
--- packages/backend/src/models/commerceFunctions.ts
+++ packages/backend/src/models/commerceFunctions.ts
@@ -1,5 +1,10 @@
 import { NewCommerce, commerce} from "@server/db/schema";
 import { db } from "@server/db/db";
+import { eq } from "drizzle-orm";
+
+export async function getAllCommerces(){
+    return await db.select().from(commerce);
+}
 
 export async function addCommerce(Commerce :NewCommerce): Promise<boolean>{
     const result = (await db.insert(commerce).values(Commerce).returning());
@@ -9,3 +14,22 @@ export async function addCommerce(Commerce :NewCommerce): Promise<boolean>{
         return false;
     }
 }
+
+export async function deleteCommerce(commerceId: string){
+    const result = (await db.delete(commerce).where(eq(commerce.id, commerceId)).returning());
+    if (result){
+        return true;
+    } else {
+        return false;
+    }
+}
+/**
+export async function updateCommerce(Commerce: commerce){
+    const result = (await db.update(commerce).set(Commerce).where(eq(commerce.id, Commerce.id)).returning());
+    if (result){
+        return true;
+    } else {
+        return false;
+    }
+}
+*/
