21e0e41cd03ad48e5c80a48c7a1bc715e37cc1e8
Esteban Fernandez <fernandezesteban344@gmail.com>
2024-09-13 00:15:05 -0300

    /product endpoint CRUD

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,12 +3,16 @@ import { logger } from "./controllers/logger";
 import swagger from "@elysiajs/swagger";
 import { loginController } from "./controllers/login";
 import { userController } from "./controllers/users";
+import { productController } from "./controllers/product";
+import { cors } from '@elysiajs/cors'; // Install with npm install @elysiajs/corsc
 
-const app = new Elysia()
+export const app = new Elysia()
+  .use(cors()) // Enable CORS
   .use(logger())
   .use(swagger())
   .use(loginController)
   .use(userController)
+  .use(productController)
   .get("/", () => "Hola bobina")
   .listen(3000);
 
