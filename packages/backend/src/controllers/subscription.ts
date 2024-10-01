4092024ecb6c1dcf53e6b5ceaf656064558fd5b8
Ignacio <ignaher12@gmail.com>
2024-10-01 13:43:49 -0300

    subscription sendEmail

--- packages/backend/src/controllers/subscription.ts
+++ packages/backend/src/controllers/subscription.ts
@@ -1,16 +1,32 @@
-import Elysia from "elysia";
-import { addSubscription } from "../models/subscriptionFunctions";
+import Elysia, { t } from "elysia";
+import {
+  addSubscription,
+  sendMailToAllSubscriptions,
+} from "../models/subscriptionFunctions";
 import { mailSubscriptionDTO } from "../types";
 export const subscriptionController = new Elysia({
   prefix: "/subscription",
-}).post(
-  "/",
-  async ({ body }) => {
-    console.log(body, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    const newSubscription = body;
-    return await addSubscription(newSubscription);
-  },
-  {
-    body: mailSubscriptionDTO,
-  },
-);
+})
+  .post(
+    "/",
+    async ({ body }) => {
+      const newSubscription = body;
+      return await addSubscription(newSubscription);
+    },
+    {
+      body: mailSubscriptionDTO,
+    },
+  )
+  .post(
+    "/sendMail",
+    async ({ body }) => {
+      const { mail_data, subject } = body;
+      await sendMailToAllSubscriptions(mail_data, subject);
+    },
+    {
+      body: t.Object({
+        mail_data: t.String(),
+        subject: t.String(),
+      }),
+    },
+  );
